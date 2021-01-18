da26902a7117dfe75ae5b2077a318353f49c820d
Jedrzej Rochala <rochala@tutanota.com>
2021-01-18 13:52:33 +0100

    fix locations

--- src/main/java/com/przewodnik/release/models/Section.java
+++ src/main/java/com/przewodnik/release/models/Section.java
@@ -14,7 +14,6 @@ public class Section {
     private Integer startToEndPoints;
     private Integer endToStartPoints;
     private Double length;
-    private String description;
 
     @ManyToOne
     @JoinColumn(name="start_id", nullable = false)
@@ -29,13 +28,12 @@ public class Section {
 
     protected Section() {}
 
-    public Section(Location start, Location end, Integer startToEndPoints, Integer endToStartPoints, Double length, String description) {
+    public Section(Location start, Location end, Integer startToEndPoints, Integer endToStartPoints, Double length) {
         this.start = start;
         this.end = end;
         this.startToEndPoints = startToEndPoints;
         this.endToStartPoints = endToStartPoints;
         this.length = length;
-        this.description = description;
     }
 
     public Long getId() {
@@ -62,10 +60,6 @@ public class Section {
         return length;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     @Override
     public String toString() {
         return "Section{" +
@@ -73,7 +67,6 @@ public class Section {
                 ", startToEndPoints=" + startToEndPoints +
                 ", endToStartPoints=" + endToStartPoints +
                 ", length=" + length +
-                ", description='" + description + '\'' +
                 ", start=" + start +
                 ", end=" + end +
                 '}';
