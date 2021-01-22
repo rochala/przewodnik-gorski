74a4fbbd27ec7307ce11ef15653d515ef62e29a2
Udek <b.duda11@wp.pl>
2021-01-22 15:31:56 +0100

    CSRF Disable

--- src/main/java/com/przewodnik/release/SecurityConfiguration.java
+++ src/main/java/com/przewodnik/release/SecurityConfiguration.java
@@ -10,6 +10,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        httpSecurity.csrf().disable();
     }
 
 }
